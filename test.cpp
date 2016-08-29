f0d36df88da03b7f7f769bec6de82639598793c5
luna <luna.lee.chiang@gmail.com>
2016-08-29 09:49:18 +0800

    -

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a =4;
+	char s='8';
+	std::cout<<a<<s<<std::endl;
+	return 0;
+}
